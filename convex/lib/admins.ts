9bb635127c1d12b8db33670038d46e9ecfeb54c5
Puff <erwinaston11704@gmail.com>
2026-06-05 21:58:34 +0800

    Update admins.ts

--- convex/lib/admins.ts
+++ convex/lib/admins.ts
@@ -2,6 +2,9 @@ export const DEFAULT_ADMIN = {
   email: 'ralphbandahala05@gmail.com',
   firstName: 'Ralph',
   lastName: 'vincent',
+  // Add a strong initial password here. 
+  // Make sure your backend hashes this when initializing the default admin!
+  password: 'ChangeMe123!', 
 } as const
 
 export function normalizeEmail(email: string) {
