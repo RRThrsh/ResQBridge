0bc0e140982263c39bdeb71f83a5460cf7efb91b
Puff <erwinaston11704@gmail.com>
2026-06-06 05:39:42 +0800

    Update AdminDomesticApproverDialog.tsx

--- src/components/admin/AdminDomesticApproverDialog.tsx
+++ src/components/admin/AdminDomesticApproverDialog.tsx
@@ -106,7 +106,7 @@ export function AdminDomesticApproverDialog({
           ),
 
         targetEmail:
-          approverRow.email,
+          approverRow!.email,
 
         firstName:
           draft.firstName,
